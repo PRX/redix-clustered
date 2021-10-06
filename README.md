7a60b820bb1bd3654ce29b552616de0b6fb3ee1a
cavis <ryancavis@gmail.com>
2021-10-06 10:15:32 -0600

    Fill out a bit more

--- README.md
+++ README.md
@@ -19,3 +19,14 @@ Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_do
 and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
 be found at [https://hexdocs.pm/redix_clustered](https://hexdocs.pm/redix_clustered).
 
+## License
+
+[MIT License](LICENSE)
+
+## Contributing
+
+1. Fork it
+2. Create your feature branch (git checkout -b my-new-feature)
+3. Commit your changes (git commit -am 'Add some feature')
+4. Push to the branch (git push origin my-new-feature)
+5. Create new Pull Request
--- mix.exs
+++ mix.exs
@@ -6,23 +6,47 @@ defmodule RedixClustered.MixProject do
       app: :redix_clustered,
       version: "0.1.0",
       elixir: "~> 1.12",
+      name: "RedixClustered",
+      source_url: "https://github.com/PRX/redix-clustered",
+      homepage_url: "https://github.com/PRX/redix-clustered",
+      description: description(),
+      package: package(),
+      docs: docs(),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:redix, "~> 1.1"},
+      {:castore, ">= 0.0.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false}
     ]
+  end
+
+  defp description do
+    "Hex package to run redix with cluster support and more"
+  end
+
+  defp package do
+    [
+      contributors: ["Ryan Cavis"],
+      maintainers: ["Ryan Cavis"],
+      licenses: ["MIT"],
+      links: %{github: "https://github.com/PRX/redix-clustered"},
+      files: ~w(lib LICENSE mix.exs README.md)
+    ]
+  end
+
+  defp docs do
+    [main: "readme", extras: ["README.md"]]
   end
 end
