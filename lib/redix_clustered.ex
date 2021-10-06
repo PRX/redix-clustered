6c93026867a4e00512bc2a51eecc5ad4bfaa0671
cavis <ryancavis@gmail.com>
2021-10-06 14:43:21 -0600

    Init commands

--- lib/redix_clustered.ex
+++ lib/redix_clustered.ex
@@ -51,4 +51,45 @@ defmodule RedixClustered do
       pre -> String.trim_leading(key, pre)
     end
   end
+
+  # singleton/unnamed cluster
+  def command(cmd), do: command(nil, cmd)
+  def pipeline(cmds), do: pipeline(nil, cmds)
+  def get(key), do: get(nil, key)
+  def get_ttl(key), do: get_ttl(nil, key)
+  def ttl(key), do: ttl(nil, key)
+  def pttl(key), do: pttl(nil, key)
+  def expire(key, ttl), do: expire(nil, key, ttl)
+  def pexpire(key, ttl), do: pexpire(nil, key, ttl)
+  def set(key, val), do: set(nil, key, val)
+  def setex(key, ttl, val), do: setex(nil, key, ttl, val)
+  def psetex(key, ttl, val), do: psetex(nil, key, ttl, val)
+  def setnx_expire_get(key, ttl, val), do: setnx_expire_get(nil, key, ttl, val)
+  def del(key), do: del(nil, key)
+  def hgetall(key), do: hgetall(nil, key)
+  def hincrby(key, flds) when is_map(flds), do: hincrby(nil, key, flds)
+  def hincrby(key, flds, ttl) when is_map(flds), do: hincrby(nil, key, flds, ttl)
+  def hincrby(key, "" <> fld, num), do: hincrby(nil, key, fld, num)
+  def scan(pattern), do: scan(nil, pattern)
+  def nuke(pattern), do: nuke(nil, pattern)
+
+  # public interface
+  defdelegate command(name, cmd), to: RedixClustered.Conn
+  defdelegate pipeline(name, cmds), to: RedixClustered.Conn
+  defdelegate get(name, key), to: RedixClustered.Commands
+  defdelegate get_ttl(name, key), to: RedixClustered.Commands
+  defdelegate ttl(name, key), to: RedixClustered.Commands
+  defdelegate pttl(name, key), to: RedixClustered.Commands
+  defdelegate expire(name, key, ttl), to: RedixClustered.Commands
+  defdelegate pexpire(name, key, ttl), to: RedixClustered.Commands
+  defdelegate set(name, key, val), to: RedixClustered.Commands
+  defdelegate setex(name, key, ttl, val), to: RedixClustered.Commands
+  defdelegate psetex(name, key, ttl, val), to: RedixClustered.Commands
+  defdelegate setnx_expire_get(name, key, ttl, val), to: RedixClustered.Commands
+  defdelegate del(name, key), to: RedixClustered.Commands
+  defdelegate hgetall(name, key), to: RedixClustered.Commands
+  defdelegate hincrby(name, key, flds), to: RedixClustered.Commands
+  defdelegate hincrby(name, key, flds, ttl), to: RedixClustered.Commands
+  defdelegate scan(name, pattern), to: RedixClustered.Commands
+  defdelegate nuke(name, pattern), to: RedixClustered.Commands
 end
--- test/redix_clustered_test.exs
+++ test/redix_clustered_test.exs
@@ -3,6 +3,14 @@ defmodule RedixClusteredTest do
 
   import RedixClustered
 
+  @name :redis_clustered_test
+  @host System.get_env("REDIS_HOST", "127.0.0.1")
+  @port System.get_env("REDIS_PORT", "6379")
+
+  setup %{case: case_name, test: test_name} do
+    [key: String.downcase("#{case_name}:#{test_name}") |> String.replace(" ", ".")]
+  end
+
   test "gets cluster names" do
     assert cluster_name([]) == :redix_clustered
     assert cluster_name(name: :my_redis) == :redix_clustered_my_redis
@@ -39,5 +47,16 @@ defmodule RedixClusteredTest do
     assert prefix(:no_prefix) == ""
     assert prefix(:no_prefix, "mykey") == "mykey"
     assert unprefix(:no_prefix, "mykey") == "mykey"
+  end
+
+  test "delegates commands", %{key: key} do
+    start_supervised!({RedixClustered, name: @name, host: @host, port: @port})
+
+    assert {:ok, _} = del(@name, key)
+    assert {:ok, nil} = get(@name, key)
+    assert {:ok, _} = setex(@name, key, 100, "value")
+    assert {:ok, ["value", num]} = get_ttl(@name, key)
+    assert num == 99 || num == 100
+    assert {:ok, _} = del(@name, key)
   end
 end
