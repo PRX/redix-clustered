d6fdeb396e8019f4f3e41ea6f47fed2e744ed213
cavis <ryancavis@gmail.com>
2021-10-06 13:49:09 -0600

    Handle prefixes

--- lib/redix_clustered.ex
+++ lib/redix_clustered.ex
@@ -5,11 +5,16 @@ defmodule RedixClustered do
 
   use Supervisor
 
+  @prefix_key "redis_prefix"
+
   def start_link(opts) do
     Supervisor.start_link(__MODULE__, opts, name: cluster_name(opts))
   end
 
   def init(opts) do
+    :ets.new(cluster_name(opts), [:set, :protected, :named_table])
+    :ets.insert(cluster_name(opts), {@prefix_key, Keyword.get(opts, :prefix)})
+
     registry_opts =
       opts
       |> Keyword.put(:name, registry_name(opts))
@@ -29,4 +34,21 @@ defmodule RedixClustered do
 
   def registry_name(opts), do: :"#{cluster_name(opts)}_registry"
   def pool_name(opts), do: :"#{cluster_name(opts)}_pool"
+
+  def prefix(name) do
+    case :ets.lookup(cluster_name(name), @prefix_key) do
+      [{@prefix_key, "" <> pre}] -> "#{pre}:"
+      _ -> ""
+    end
+  end
+
+  def prefix(name, key), do: "#{prefix(name)}#{key}"
+  def unprefix(name, keys) when is_list(keys), do: Enum.map(keys, &unprefix(name, &1))
+
+  def unprefix(name, key) do
+    case prefix(name) do
+      "" -> key
+      pre -> String.trim_leading(key, pre)
+    end
+  end
 end
--- test/redix_clustered_test.exs
+++ test/redix_clustered_test.exs
@@ -1,8 +1,43 @@
 defmodule RedixClusteredTest do
-  use ExUnit.Case
-  doctest RedixClustered
+  use ExUnit.Case, async: true
 
-  test "greets the world" do
-    assert RedixClustered.hello() == :world
+  import RedixClustered
+
+  test "gets cluster names" do
+    assert cluster_name([]) == :redix_clustered
+    assert cluster_name(name: :my_redis) == :redix_clustered_my_redis
+    assert cluster_name(nil) == :redix_clustered
+    assert cluster_name(:my_redis) == :redix_clustered_my_redis
+  end
+
+  test "gets registry names" do
+    assert registry_name([]) == :redix_clustered_registry
+    assert registry_name(name: :my_redis) == :redix_clustered_my_redis_registry
+    assert registry_name(nil) == :redix_clustered_registry
+    assert registry_name(:my_redis) == :redix_clustered_my_redis_registry
+  end
+
+  test "gets pool names" do
+    assert pool_name([]) == :redix_clustered_pool
+    assert pool_name(name: :my_redis) == :redix_clustered_my_redis_pool
+    assert pool_name(nil) == :redix_clustered_pool
+    assert pool_name(:my_redis) == :redix_clustered_my_redis_pool
+  end
+
+  test "handles prefixes" do
+    start_supervised!({RedixClustered, name: :with_prefix, prefix: "some-thing"})
+
+    assert prefix(:with_prefix) == "some-thing:"
+    assert prefix(:with_prefix, "mykey") == "some-thing:mykey"
+    assert unprefix(:with_prefix, "some-thing:mykey") == "mykey"
+    assert unprefix(:with_prefix, "mykey") == "mykey"
+  end
+
+  test "handles non-prefixes" do
+    start_supervised!({RedixClustered, name: :no_prefix})
+
+    assert prefix(:no_prefix) == ""
+    assert prefix(:no_prefix, "mykey") == "mykey"
+    assert unprefix(:no_prefix, "mykey") == "mykey"
   end
 end
